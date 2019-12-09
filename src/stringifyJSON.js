cd6e498bb3a703a890a010f106062273a7a70032
willchen13 <williamchen13225@gmail.com>
2019-12-09 11:57:42 -0800

    almost completed jsonstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,49 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // check, 9, null, true, false
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var result='[';
+    for (var i=0; i<obj.length; i++) {
+      if (i!==obj.length-1) {
+        result += stringifyJSON(obj[i]) + ',';
+      } else {
+        result += stringifyJSON(obj[i]);
+      }
+    }
+    return result + ']';
+  }
+
+  if (typeof obj === 'object') {
+    var result = '{';
+    var count = 0;
+    for (var key in obj) {
+      var stringKey = stringifyJSON(key);
+      var stringValue = stringifyJSON(obj[key]);
+      count++;
+      if (count !== Object.keys(obj).length) {
+        result += stringKey + ':' + stringValue + ',';
+      } else {
+        result += stringKey + ':' + stringValue;
+      }
+    }
+    return result + '}';
+  }
+
 };
